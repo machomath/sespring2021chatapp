5ef0761fd003770ba541bb3a8bc33b9b836f7c04
Dr. Zeb <aristopedia1@gmail.com>
2021-03-25 15:24:37 -0500

    Multi views created

--- app.js
+++ app.js
@@ -7,10 +7,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 // app.set('views', 'views');//unnecessary
 
-app.use("/", (req, res, next)=>{
+app.get("/home", (req, res, next)=>{
     res.render("index", {title: "Home"});
 });
 
+app.get("/about", (req, res, next)=>{
+    res.render("about", {title: "About"});
+});
+
+app.get("/chat-room", (req, res, next)=>{
+    res.render("chat-room", {title: "Chat Room"});
+});
+
+app.get("/me", (req, res, next)=>{
+    res.render("me", {title: "Me"});
+});
+
+app.get("/", (req, res, next)=>{
+    res.render("index", {title: "Home"});
+});
 
 app.listen(3000, ()=>{
     console.log("Server running at port 3000");
