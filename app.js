dea4d7eb5ce1da6011b94ce2f941f260a312e046
Dr. Zeb <aristopedia1@gmail.com>
2021-03-27 16:35:55 -0500

    Every thing before DB

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ app.use(express.urlencoded({extended: true})); //to reach POST parameters
 ////////////System Settings above this line ////////////////////
 
 app.use(publicRouts);
-let user1 = new User(1,"John", "s1@gmail.com", "122334");
+try {
+    let user1 = new User(100,"John", "s1@gmail.com", "122334");
+    user1.id = 200
+    console.log(user1.id);
+    user1.email = "abc"
+    user1.id = -2000;
+} catch (error) {
+    console.log(error);
+}
+
 
 ///////////Routs above this line////////////////////////////
 app.listen(3000, ()=>{
--- models/user.js
+++ models/user.js
@@ -1,14 +1,42 @@
 const DBObject = require("./dbobject");
 module.exports = class User extends DBObject{
-    // #id = 1;
-    constructor(_id, _fullName, _email, _password){
-        super();
-        this.id = _id;
-        this.fullName = _fullName;
-        this.email = _email;
-        this.password = _password;
+    #id;
+    #email;
+    constructor(_id, _fullName, _em, _password){
+        try {
+            super();
+            this.id = _id; //this is a call to setter for id
+            this.fullName = _fullName;
+            this.email = _em; //this is a call to setter
+            this.password = _password;
+        } catch (error) {
+            throw error;
+        }
+        
+    }
+    get email(){
+        return this.#email;
     }
 
+    set email(em){
+        if(em.split("@").length === 2){
+            this.#email = em;
+        }else{
+            throw "Invalid Email";
+        }
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    set id(_id){
+        if(typeof _id === "number" && _id > 0){
+            this.#id = _id;
+        }else{
+            throw "Invalid Id";
+        }
+    }
      
 
 }
